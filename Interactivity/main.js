c7656585e0b2cc154759fc99b97dea04f5514a4c
dDobrevv <dobromir.s.dobrev@gmail.com>
2020-01-21 18:10:34 +0200

    Added sending data to localStorage

--- Interactivity/main.js
+++ Interactivity/main.js
@@ -1,3 +1,29 @@
+// Send data to localStorage
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+function saveIssue(e) {
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueId = chance.guid();
+    var issueStatus = 'Open';
+
+    // Creating an issue object
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    // Inserting the object in localStorage
+    if (localStorage.getItem('issues') == null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+}
+
 // Fetching the list of issues that already available. Using browser local storage.
 function fetchIssues() {
     var issues = JSON.parse(localStorage.getItem('issues'));
@@ -5,7 +31,7 @@ function fetchIssues() {
 
     issuesLists.innerHTML = '';
 
-    for (var i = 0; i < issues.length; i++){
+    for (var i = 0; i < issues.length; i++) {
         var id = issues[i].id;
         var desc = issues[i].description;
         var severity = issues[i].severity;
@@ -23,3 +49,4 @@ function fetchIssues() {
         '</div';
     }
 }
+
